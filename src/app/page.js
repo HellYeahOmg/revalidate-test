a7b7348cdd2c23c8308407b96a078b479329d55e
Ali Raza <alirazadev27@gmail.com>
2024-07-22 20:33:46 +0500

    FIXED: removed cache:'no-cache' to force revalidation

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,11 @@ export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
 export default async function Home() {
-  const data = await fetch('http://localhost:3000/api', { cache: 'no-store', next: { tags: ['data'] } })
+  const data = await fetch('http://localhost:3000/api', { next: { tags: ['data'] } })
     .then(res => res.json())
 
   return (
-    <main>
+    <main className="bg-black">
       <div className={'mb-8'}>
         <p>data list:</p>
         {data.map(item =>
